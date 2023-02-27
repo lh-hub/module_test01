6921f6cf6cc913e5c9d6f7b1b32976c24094e92f
os-lihui01 <os-lihui01@jsfund.cn>
2023-02-27 15:28:10 +0800

    add hello package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lh-hub/module_test01
+
+go 1.19
---
+++ hello.go
@@ -0,0 +1,7 @@
+package module_test01
+
+import "fmt"
+
+func Hello(name string) string{
+	return fmt.Sprintf("Hello, %s", name)
+}
